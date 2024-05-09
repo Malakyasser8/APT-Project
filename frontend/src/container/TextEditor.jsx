db8eb60ea5605380d91b55363a2fc4055c51b20a
Malakyasser8 <malakyasser8@gmail.com>
2024-05-10 01:12:00 +0300

    Added text editor and websocket server

--- frontend/src/container/TextEditor.jsx
+++ frontend/src/container/TextEditor.jsx
@@ -8,16 +8,74 @@ let version = 0;
 let changesQueue = [];
 
 const TextEditor = ({ fileContent }) => {
-  const [quill, setQuill] = useState(fileContent);
+  // const [quill, setQuill] = useState(fileContent);
   // const quillRef = useRef(null);
+  const quillRef = useRef(null);
 
   useEffect(() => {
-    if (!quill) {
+    if (!quillRef.current) {
       const editor = new Quill("#editor-container", {
         theme: "snow",
         placeholder: "Write something...",
       });
-      setQuill(editor);
+      if (fileContent) {
+        editor.root.innerHTML = fileContent;
+      }
+      quillRef.current = editor;
+    }
+
+    if (quillRef.current) {
+      // Attach event listener to detect text change in Quill editor
+      quillRef.current.on("text-change", function (delta, oldDelta, source) {
+        console.log("DELTA", delta.ops);
+        let isDelete = false;
+        let isInsert = false;
+        let isBold = false;
+        let isItalic = false;
+        let currentIndex = 0;
+        delta.ops.forEach((operation) => {
+          if (operation.delete) {
+            isDelete = true;
+          }
+          if (operation.retain && !operation.attributes) {
+            currentIndex += operation.retain;
+          }
+          if (operation.insert) {
+            isInsert = true;
+          }
+          if (
+            operation.attributes &&
+            operation.attributes.hasOwnProperty("bold")
+          ) {
+            const boldValue = operation.attributes.bold;
+            if (boldValue == true) {
+              isBold = true;
+            } else if (boldValue == false) {
+              isBold = false;
+            } else {
+              isBold = null; // Handle other cases (e.g., null)
+            }
+          }
+          if (
+            operation.attributes &&
+            operation.attributes.hasOwnProperty("italic")
+          ) {
+            const val = operation.attributes.italic;
+            if (val == true) {
+              isItalic = true;
+            } else if (val == false) {
+              isItalic = false;
+            } else {
+              isItalic = null; // Handle other cases (e.g., null)
+            }
+          }
+        });
+        if (source == "user" && source != "api") {
+          console.log("in handle change");
+          handleTextChange(isDelete, isBold, isItalic, isInsert, currentIndex);
+          oldContent = quillRef.current.root.innerHTML;
+        }
+      });
     }
     // Setup WebSocket connection on component mount (equivalent to window.onload)
     setupWebSocket();
@@ -32,19 +90,7 @@ const TextEditor = ({ fileContent }) => {
         editor.destroy();
       }
     };
-  }, [fileContent]); // Empty dependency array to run only once on mount (window load)
-
-  useEffect(() => {
-    if (quill) {
-      // Handle text change in Quill editor
-      quill.on("text-change", (delta, oldDelta, source) => {
-        if (source === "user") {
-          handleTextChange(delta);
-        }
-      });
-    }
-  }, [quill]); // Run effect when quill instance changes
-
+  }, []); // Empty dependency array to run only once on mount (window load)
   function setupWebSocket() {
     const url = "ws://localhost:3001/chat";
     webSocket = !webSocket ? new WebSocket(url) : webSocket;
@@ -87,7 +133,7 @@ const TextEditor = ({ fileContent }) => {
         });
       }
       version++;
-      oldContent = quill.root.innerHTML;
+      oldContent = quillRef.current.root.innerHTML;
     };
 
     webSocket.onclose = function (event) {
@@ -103,18 +149,25 @@ const TextEditor = ({ fileContent }) => {
     isItalic,
     isInsert
   ) {
-    console.log("HII");
+    console.log("HII", isBold);
 
-    if (isBold && !isInsert) {
-      quill.formatText(index, 1, "bold", true);
+    if (isBold == null) {
+      quillRef.current.formatText(index, 1, "bold", false);
+    } else if (isItalic == null) {
+      quillRef.current.formatText(index, 1, "italic", false);
+    } else if (isBold && !isInsert) {
+      quillRef.current.formatText(index, 1, "bold", true);
     } else if (isItalic && !isInsert) {
-      quill.formatText(index, 1, "italic", true);
+      quillRef.current.formatText(index, 1, "italic", true);
     } else {
-      let text = quill.root.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
+      let text = quillRef.current.root.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
       let length = text.length;
       let formatsArray = [];
       for (let i = 0; i < length; i++) {
-        formatsArray.push({ char: text[i], format: quill.getFormat(i, 1) });
+        formatsArray.push({
+          char: text[i],
+          format: quillRef.current.getFormat(i, 1),
+        });
       }
       if (isDelete) {
         text = text.slice(0, index) + text.slice(index + 1);
@@ -125,20 +178,20 @@ const TextEditor = ({ fileContent }) => {
           format: { italic: isItalic, bold: isBold },
         });
       }
-      quill.setText(text, "api");
+      quillRef.current.setText(text, "api");
       length = text.length;
       for (let i = 0; i < length; i++) {
-        quill.formatText(i, 1, formatsArray[i].format);
+        quillRef.current.formatText(i, 1, formatsArray[i].format);
       }
     }
 
     // Set the cursor position to the end of the editor
-    quill.setSelection(index + 1);
+    quillRef.current.setSelection(index + 1);
   }
 
   function findChanges(isDelete, isBold, isItalic, isInsert, currentIndex) {
     // const quill = quillRef.current.getEditor();
-    const newContent = quill.root.innerHTML;
+    const newContent = quillRef.current.root.innerHTML;
     const strippedOldContent = oldContent.replace(/<\/?[^>]+(>|$)/g, ""); //mal
     const strippedNewContent = newContent.replace(/<\/?[^>]+(>|$)/g, ""); //ml
     let character = strippedNewContent[currentIndex]; //abc
@@ -155,7 +208,7 @@ const TextEditor = ({ fileContent }) => {
       isInsert,
       version,
     });
-    oldContent = quill.root.innerHTML;
+    oldContent = quillRef.current.root.innerHTML;
     return changes;
   }
 
@@ -181,44 +234,12 @@ const TextEditor = ({ fileContent }) => {
     }
   }
 
-  const handleEditorChange = (html) => {
+  const handleEditorChange = () => {
+    console.log("in editpr");
     // const cleanText =
     //   new DOMParser().parseFromString(html, "text/html").body.textContent || "";
     // setEditorHtml(html);
     // const quill = quillRef.current?.getEditor();
-    if (quill) {
-      // Attach event listener to detect text change in Quill editor
-      quill.on("text-change", function (delta, oldDelta, source) {
-        console.log("DELTA", delta.ops);
-        let isDelete = false;
-        let isInsert = false;
-        let isBold = false;
-        let isItalic = false;
-        let currentIndex = 0;
-        delta.ops.forEach((operation) => {
-          if (operation.delete) {
-            isDelete = true;
-          }
-          if (operation.retain && !operation.attributes) {
-            currentIndex += operation.retain;
-          }
-          if (operation.insert) {
-            isInsert = true;
-          }
-          if (operation.attributes?.bold) {
-            isBold = true;
-          }
-          if (operation.attributes?.italic) {
-            isItalic = true;
-          }
-        });
-        if (source == "user" && source != "api") {
-          console.log("in handle change");
-          handleTextChange(isDelete, isBold, isItalic, isInsert, currentIndex);
-          oldContent = quill.root.innerHTML;
-        }
-      });
-    }
   };
 
   return (
